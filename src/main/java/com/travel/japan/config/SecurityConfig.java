d3c35a9243c6a227c8b846461b60f9b2771f8b24
yunsy1103 <yunsy1103@naver.com>
2024-06-15 16:33:16 +0900

    config 수정 및 설정 추가

--- src/main/java/com/travel/japan/config/SecurityConfig.java
+++ src/main/java/com/travel/japan/config/SecurityConfig.java
@@ -1,9 +1,20 @@
 package com.travel.japan.config;
 
+import com.travel.japan.jwt.JwtFilter;
+import com.travel.japan.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
