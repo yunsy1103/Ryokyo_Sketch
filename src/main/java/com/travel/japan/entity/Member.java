7e8b587f27ce5e0246ddf215df83ce256b4283bb
yunsy1103 <yunsy1103@naver.com>
2024-09-20 12:56:40 +0900

    entity 설정 변경

--- src/main/java/com/travel/japan/entity/Member.java
+++ src/main/java/com/travel/japan/entity/Member.java
@@ -27,26 +27,28 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 45, unique = true)
+    @Column(length = 50, unique = true)
     private String email;
 
-    @Column(length = 100)
+    @Column(length = 255)
     private String password;
 
-    @Column(length = 50)
+    @Column(length = 255)
     private String nickname;
 
+    @Column(length = 10)
     private String gender;
 
-    @Column(length = 45)
+    @Column(length = 50)
     private String birth;
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 10)
+    @Column(length = 50)
     private Nationality nationality;
 
-
-
+    // 상태 필드 추가
+    @Column(length = 255, nullable = false)
+    private String status = "active";  // 기본값을 'active'로 설정
 
     public void encodePassword(PasswordEncoder passwordEncoder){
         this.password = passwordEncoder.encode(password);
